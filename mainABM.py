2474fca1a6093888b22bac6947f4e06e72c843d9
Bertram Hojer <ber.hoejer@gmail.com>
2020-04-29 17:13:11 +0200

    rules added; 'explore', 'similarity_check', 'interaction_time', 'conversation-time'

--- mainABM.py
+++ mainABM.py
@@ -11,13 +11,17 @@ import matplotlib.pyplot as plt
 import random
 # for managing the data-frame produced by the simulation
 import pandas as pd
+# data handling
+import numpy as np
 
 # import dataset
 from dataPrep import iqr, mad, pause, speechrate
 
+import rulebook
+
 
 # defining the agent class
-class Agent(Agent, Grid):
+class Agent(Agent):
 	# An agent-cass inheriting the properties of Agent
 	# define properties needed to specify an object of class Agent()
 	def __init__(self, unique_id, model):
@@ -27,73 +31,65 @@ class Agent(Agent, Grid):
 		to specify any variables that we make up that are manipulated by interaction rules. So if an initial 
 		value is manipulated over time we have to specify that in the start a well. """
 
-		#self.iqr = 0
-		#self.speechrate = 0
-		#self.mad = 0
-		#self.pause = 0
-
 		# specify activity-level
 		self.status = "Active"
 
 		# specify agent-properties
-		self.interaction_n = 0
+		self.unique_interactions = 0
 		self.interaction_time = 0
 		self.conversation_time = 0
 		self.alignment = 0
 		self.sociality = 0
 
-	# define function for checking the environment and moving
+	
 	def move(self):
+		# examine environment
 		possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
-		for i in range(8):
-			new_step = self.model.grid.get_neighbors(pos = possible_steps[i], moore=True, include_center=False, radius = 1)
-			if len(new_step) > 1:
-				pass
-			else:
-				new_position = self.random.choice(possible_steps)
-				self.model.grid.move_agent(self, new_position)
-				if len(new_position) > 2:
-					new_position = self.random.choice(possible_steps)
-					self.model.grid.move_agent(self, new_position)
-			
-		#print(len(new_step))
+		# choose random cells in neighbor grid
+		new_position = self.random.choice(possible_steps)
+		# move agent to new cell
+		self.model.grid.move_agent(self, new_position)
 
-    # defining functions for rule-based interaction
-    # rule based on iqr
-	def ruleOne(self):
-    	# check the neighbourhood for agents
-		cellmates = self.model.grid.get_cell_list_contents([self.pos])
-    	# specify other based on amount of agents in the cell
-		other = self.random.choice(cellmates)
+		# get cell-contents
+		cell_info = self.model.grid.get_cell_list_contents([self.pos])
 
-        # interaction rule
-		if self.iqr < other.iqr:
-			self.alignment += 0.1
-			other.alignment -= 0.1
+		# if the cell contains more than 2 agents already, repeat the movement
+		while len(cell_info) > 1:
+			# examine environment
+			possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+			# choose random cell in new neighbor grid
+			new_position = self.random.choice(possible_steps)
+			# get new grid info to avoid infinite recursion
+			cell_info = self.model.grid.get_cell_list_contents([self.pos])
+			# move the agent
+			self.model.grid.move_agent(self, new_position)
 
-    # rule based on rate
-	def ruleTwo(self):
-    	# check the neighbourhood for agents
-		cellmates = self.model.grid.get_cell_list_contents([self.pos])
-    	# specify other based on amount of agents in the cell
-		other = self.random.choice(cellmates)
-		other_overall = float(other.iqr + other.speechrate + other.mad + other.pause)
-		self_overall = float(self.iqr + self.speechrate + self.mad + self.pause)
-		print(other_overall)
-		print(self_overall)
-		print("self_ID " + str(self.unique_id))
 
-        # interaction rule
-		if self.speechrate > other.speechrate:
-			self.sociality += 0.1
-			other.sociality -= 0.1
 
     # define step function - what the agent does for each timestep
 	def step(self):
+
+		cell = self.model.grid.get_cell_list_contents([self.pos])
+		
+		if len(cell) == 2:
+			other = self.random.choice(cell)
+			while other == self:
+				other = self.random.choice(cell)
+				
+
+			rulebook.interaction_time(self, other = other)
+			rulebook.conversation_time(self, other = other)
+			
+			rulebook.similarity_check(agent = self, other = other)
+
+			print(other.iqr)
+			print(self.iqr)
+
+		elif len(cell) != 2:
+			rulebook.explore(self)
+
 		if self.status == "Active":
 			self.move()
-		self.ruleOne()
-		self.ruleTwo()
 
 
 
@@ -126,24 +122,21 @@ class Model(Model):
 
 			# add data-collector to the agent
 			self.datacollector = DataCollector(
-				agent_reporters = {"Variability": "iqr",
-									"Speechrate": "speechrate",
-									"MAD": "mad",
-									"PauseFreq": "pause",
-									"Alignment": "alignment",
-									"Sociality": "sociality"})
+				agent_reporters = {"interactions": "unique_interactions",
+									"interaction_time": "interaction_time",
+									"conversation_time": "conversation_time"})
 
 	def step(self):
 		# advance the model and collect data
 		self.datacollector.collect(self)
 		self.schedule.step()
 
-model = Model(2, 4, 4)
-for i in range(2):
+model = Model(50, 10, 10)
+for i in range(50):
 	model.step()
 
 data = model.datacollector.get_agent_vars_dataframe()
-data.to_csv("asdABM.csv")
+data.to_csv("data.csv")
 print("CSV written")
 
 
--- rulebook.py
+++ rulebook.py
@@ -9,28 +9,68 @@ from mesa.datacollection import DataCollector
 import random
 # for managing the data-frame produced by the simulation
 import pandas as pd
+# data handling
+import numpy as np
 # for creating binomial distributions aiding decision-making
 from scipy.stats import binom
-# plotting
-import matplotlib.pyplot as plt
 
 
 
-def similarity_check(current_agent, other_agent):
-    if abs(other_overall-self_overall) <= 0.5:
-        if binom.rvs(n = 1, p = 0.3) == 1:
-            self.status = "Inactive"
-            self.conversation_time += 1
-        else: 
-            self.status = "Active"
+# checking whether the agents are similar and estimating a probability of moving to a new grid
+def similarity_check(agent, other):
+    iqr_diff = np.absolute(agent.iqr - other.iqr)
+    mad_diff = np.absolute(agent.mad - other.mad)
+    speechrate_diff = np.absolute(agent.speechrate - other.speechrate)
+    pause_diff = np.absolute(agent.pause - other.pause)
+
+    # probability of moving
+    movement_prob = ((0.125 * agent.iqr + 0.125 * iqr_diff) + 
+                    (0.125 * agent.mad + 0.125 * mad_diff) +
+                    (0.125 * (1 - agent.speechrate) + 0.125 * speechrate_diff) +
+                    (0.125 * agent.pause + 0.125 * pause_diff))
+
+    rand_n = float(random.random())
+    if movement_prob > rand_n:
+        agent.status = "Active"
+    else:
+        agent.status = "Inactive"
+    #print(agent.status)
+    #print(movement_prob)
 
 
+# If alone on a cell, set status to active with probability based on pitch-scores
+def explore(agent):
+    # probability of moving
+    movement_prob = ((0.125 * (1 - agent.iqr)) + 
+                    (0.125 * (1 - agent.mad)) +
+                    (0.125 * agent.speechrate) +
+                    (0.125 * (1 - agent.pause)))
+
+    rand_n = float(random.random())
+    if movement_prob > rand_n:
+        agent.status = "Active"
+    else:
+        agent.status = "Inactive"
 
 
-def rule_one():
-    # get info on other agents in the cell
-    cellmates = self.model.grid.get_cell_list_contents([self.pos])
-    # if there are more than one agent choose either one of them
-    other = self.random.choice(cellmates)
-    # run similarity check
-    similarity_check(current_agent = self, other_agent = other)
+# Counting the amount of time an agent has interacted
+def interaction_time(agent, other):
+    cell_info = agent.model.grid.get_cell_list_contents([agent.pos])
+    if len(cell_info) == 2:
+        agent.interaction_time += 1
+        if agent.status == "Active" or other.status == "Active":
+            agent.unique_interactions += 1
+
+
+# Counting conversation time
+def conversation_time(agent, other):
+    # calculating the ratio between agents pause frequency
+    if agent.pause > other.pause:
+        # estimating conversation time
+        pause_ratio = (agent.pause / other.pause)
+        agent.conversation_time += 1 - (1 / pause_ratio)
+    elif agent.pause < other.pause:
+        # estimating conversation time
+        pause_ratio = (other.pause / agent.pause)
+        agent.conversation_time += (1 / pause_ratio)
+
